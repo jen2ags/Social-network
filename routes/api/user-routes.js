3383af1e8da8cf6e7664c5fc219a1bbebebaa399
jennj <jennjenn93@gmail.com>
2022-06-09 16:26:50 -0500

    adding baseline for controllers

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {
-    getAllUser,
+    getAllUsers,
     getUserById,
     createUser,
     updateUser,
@@ -12,7 +12,7 @@ const {
 //set up GET all and POST at /api/users
 router
 .route('/')
-.get(getAllUser)
+.get(getAllUsers)
 .post(createUser);
 
 //set up GET one, PUT, and DELETE at /api/users/:id
