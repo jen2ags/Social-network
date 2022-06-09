bf23dfc4521a3c072bd94d3416317f787745bc37
jennj <jennjenn93@gmail.com>
2022-06-09 15:04:51 -0500

    adding thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,62 +1,42 @@
-const { Schema, model, Types } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const moment = require('moment');
 
-const ReplySchema = new Schema(
+
+const ThoughtSchema = new Schema(
     {
-        replyId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        replyBody: {
+        thoughtText: {
             type: String,
             required: true,
-            trim: true
-        },
-        writtenBy: {
-            type: String,
-            required: true
+            maxlength: 280,
+            minlength: 1
+            
         },
         createdAt: {
             type: Date,
             default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        }
+            /*get: createdAtVal => *\
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+
     },
     {
         toJSON: {
+            virtuals: true,
             getters: true
-        }
+        },
+        id: false
     }
 );
 
-const CommentSchema = new Schema ({
-    writtenBy: {
-        type: String
-    },
-    commentBody: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
-    },
-    replies: [ReplySchema]
-},
-{
-    toJSON: {
-        virtuals: true,
-        getters: true
-    },
-    id: false
-}
-);
-
-CommentSchema.virtual('replyCount').get(function() {
-    return this.replies.length;
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
 });
 
-const Comment = model('Comment', CommentSchema);
+const Thought = model('Thought', ThoughtSchema);
 
-module.exports = Comment;
+module.exports = Thought;
