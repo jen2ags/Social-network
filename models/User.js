186da7737a96119b591d0a52adfd85ec5901146c
jennj <jennjenn93@gmail.com>
2022-06-09 12:21:42 -0500

    add user model

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema } = require('mongoose');
+
+{ Schema, model } = require('mongoose');
+
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is Required',
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'Email is Required',
+        unique: true,
+        match: [/.+\@.+\..+/]
+        
+    },
+    thoughts: [
+        { 
+            type: Schema.Types.ObjectId, 
+            ref: 'Thought' 
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+    
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+//get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+//create the Pizza model using PizzaSchema
+const User = model('User', UserSchema);
+
+//export the Pizza model
+module.exports = User;
