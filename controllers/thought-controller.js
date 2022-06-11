f9b2f3e553e4ed31160d48f333e8e8eb3e3459b1
jennj <jennjenn93@gmail.com>
2022-06-11 13:40:50 -0500

    editing get thought by id

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -5,41 +5,33 @@ const thoughtController = {
     //get all thoughts
     getAllThoughts(req, res) {
         Thought.find({})
-            .populate([
-                {
-                    path: 'thoughts',
-                    select: '-__v'
-                },
+            .populate(
                 {
                     path: 'reactions',
                     select: '-__v'
                 }
-            ])
+            )
             .select('-__v')
             .sort({ _id: -1 })
-            .then(dbThoughtData => res.json(dbThoughtData))
+            .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 console.log(err);
-                res.status(400).json(err);
+                res.status(500).json(err);
             });
     },
 
     //get one thought by id
     getThoughtById({ params }, res) {
-        Thought.findOne({ _id: params.id })
-            .populate([
-                {
-                    path: 'thoughts',
-                    select: '-__v'
-                },
+        Thought.findOne({ _id: params.thoughtId })
+            .populate(
                 {
                     path: 'reactions',
                     select: '-__v'
                 }
-            ])
+            )
             .select('-__v')
-            .then(dbThoughtData => {
-                if (!dbThoughtData) {
+            .then(dbUserData => {
+                if (!dbUserData) {
                     res.status(404).json({ message: 'No thought found with this id!' });
                     return;
                 }
